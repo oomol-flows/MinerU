32b21442617d36614368d3ea61d540ee7a603e1a
TaoZeyu <i@taozeyu.com>
2025-01-07 12:47:38 +0800

    feat(blocks): add generator

--- shared/converter/generator.py
+++ shared/converter/generator.py
@@ -21,29 +21,37 @@ class Generator:
     self._output_image_path: str = output_image_path
     self._lock: Lock = Lock()
 
-  def draw_layout(self, name: str):
+  def draw_layout(self, name: str | None) -> str:
+    output_path = self._to_output_path(name, "_layout.pdf")
     with self._lock:
-      self._pipe_result.draw_layout(self._to_output_path(name, "_layout.pdf"))
+      self._pipe_result.draw_layout(output_path)
+    return output_path
 
-  def draw_span(self, name: str):
+  def draw_span(self, name: str | None) -> str:
+    output_path = self._to_output_path(name, "_spans.pdf")
     with self._lock:
-      self._pipe_result.draw_span(self._to_output_path(name, "_spans.pdf"))
+      self._pipe_result.draw_span(output_path)
+    return output_path
 
-  def dump_md(self, name: str):
+  def dump_md(self, name: str | None) -> str:
+    output_path = self._to_output_path(name, ".md")
     with self._lock:
       self._pipe_result.dump_md(
         self._md_writer,
-        self._to_output_path(name, ".md"),
+        output_path,
         self._output_image_path,
       )
+    return output_path
 
-  def dump_content_list(self, name: str):
+  def dump_content_list(self, name: str | None) -> str:
+    output_path = self._to_output_path(name, "_content_json.json")
     with self._lock:
       self._pipe_result.dump_content_list(
         self._md_writer,
-        self._to_output_path(name, "_content_json.json"),
+        output_path,
         self._output_image_path,
       )
+    return output_path
 
   def _to_output_path(self, name: str | None, sufix: str) -> str:
     if name is None:
