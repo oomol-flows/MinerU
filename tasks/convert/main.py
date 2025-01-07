99812a193eb64baff9e9c7a35e00292addd2a80d
TaoZeyu <i@taozeyu.com>
2025-01-07 11:31:23 +0800

    feat(block): support device type

--- tasks/convert/main.py
+++ tasks/convert/main.py
@@ -1,7 +1,8 @@
 import os
 import platform
-import shutil
+import json
 
+from typing import Literal
 from magic_pdf.data.data_reader_writer import FileBasedDataWriter, FileBasedDataReader
 from magic_pdf.data.dataset import PymuDocDataset
 from magic_pdf.model.doc_analyze_by_custom_model import doc_analyze
@@ -24,16 +25,22 @@ def main(params: dict, context: Context):
   return { "output_path": output_path }
 
 def _apply_config(params: dict):
-  template_path: str = params["config_path"] # TODO update
-  config_path: str = os.path.join("~", "magic-pdf.json")
+  template_path: str = params["config_path"]
+  device: Literal["cpu", "cuda"] = params["device"]
+
+  with open(template_path, "r", encoding="utf-8") as file:
+    config_json: dict = json.load(file)
+    config_json["device-mode"] = device
+
+  config_path: str = os.path.join(os.path.expanduser("~"), "magic-pdf.json")
   config_path = os.path.abspath(config_path)
-  shutil.copy(template_path, config_path)
+
+  with open(config_path, "w", encoding="utf-8") as file:
+    json.dump(config_json, file, ensure_ascii=False, indent=2)
 
 def _convert_pdf(pdf_path: str, output_path: str):
   pdf_file_name = os.path.basename(pdf_path)
   name_without_suff = pdf_file_name.split(".")[0]
-
-  # prepare env
   output_image_path = os.path.join(output_path, "images")
   image_dir = str(os.path.basename(output_image_path))
 
@@ -49,12 +56,12 @@ def _convert_pdf(pdf_path: str, output_path: str):
   ds = PymuDocDataset(pdf_bytes)
 
   if ds.classify() == SupportedPdfParseMethod.OCR:
-      infer_result = ds.apply(doc_analyze, ocr=True)
-      pipe_result = infer_result.pipe_ocr_mode(image_writer)
+    infer_result = ds.apply(doc_analyze, ocr=True)
+    pipe_result = infer_result.pipe_ocr_mode(image_writer)
 
   else:
-      infer_result = ds.apply(doc_analyze, ocr=False)
-      pipe_result = infer_result.pipe_txt_mode(image_writer)
+    infer_result = ds.apply(doc_analyze, ocr=False)
+    pipe_result = infer_result.pipe_txt_mode(image_writer)
 
   infer_result.draw_model(os.path.join(output_path, f"{name_without_suff}_model.pdf"))
   pipe_result.draw_layout(os.path.join(output_path, f"{name_without_suff}_layout.pdf"))
