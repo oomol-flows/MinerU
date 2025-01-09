2be59f855a5248863be604785202d93552d10e33
TaoZeyu <i@taozeyu.com>
2025-01-09 18:51:39 +0800

    feat(project): add absolute_image_path field

--- shared/converter/generator.py
+++ shared/converter/generator.py
@@ -11,19 +11,21 @@ class Generator:
       md_writer: Any,
       proto_name: str,
       output_path: str,
-      output_image_path: str,
+      image_folder: str,
     ) -> None:
 
     self._proto_name: str = proto_name
     self._pipe_result: Any = pipe_result
     self._md_writer: Any = md_writer
     self._output_path: str = output_path
-    self._output_image_path: str = output_image_path
+    self._image_folder: str = image_folder
     self._lock: Lock = Lock()
 
-  @property
-  def output_image_path(self) -> str:
-    return self._output_image_path
+  def output_image_path(self, absolute_image_path: bool) -> str:
+    if absolute_image_path:
+      return os.path.join(self._output_path, self._image_folder)
+    else:
+      return os.path.join(".", self._image_folder)
 
   def draw_layout(self, name: str | None) -> str:
     output_path = self._to_output_path(name, "_layout.pdf")
@@ -37,23 +39,23 @@ class Generator:
       self._pipe_result.draw_span(output_path)
     return output_path
 
-  def dump_md(self, name: str | None) -> str:
+  def dump_md(self, name: str | None, absolute_image_path: bool) -> str:
     output_path = self._to_output_path(name, ".md")
     with self._lock:
       self._pipe_result.dump_md(
         self._md_writer,
         output_path,
-        self._output_image_path,
+        self.output_image_path(absolute_image_path),
       )
     return output_path
 
-  def dump_content_list(self, name: str | None) -> str:
+  def dump_content_list(self, name: str | None, absolute_image_path: bool) -> str:
     output_path = self._to_output_path(name, "_content_json.json")
     with self._lock:
       self._pipe_result.dump_content_list(
         self._md_writer,
         output_path,
-        self._output_image_path,
+        self.output_image_path(absolute_image_path),
       )
     return output_path
 
